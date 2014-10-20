e400b8863809dc099ae0d3eaa1eaf8d039ca0b20
anishbharatha <anish.bharatha@gmail.com>
2014-10-20 13:39:25 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AbbThoughts::Application.config.secret_key_base = 'd200ff57539c9b7c682ca16f8a5b57b4b8cc13fbdfe8bc4ca64791f472399a8746935c3d8440a369ded161b90b03964ea861e19283761f007f2043b3f116fb25'
---
+++ spec/requests/tasks_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe "Tasks", :type => :request do
+  describe "GET /tasks" do
+    it "display some tasks" do
+    	visit tasks_path
+    	page.should have_content 'Awake !!'
+  end
+end
